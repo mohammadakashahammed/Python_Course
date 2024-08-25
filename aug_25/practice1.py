d68b4962fe89d8489aa5fc8fc3758dd8b64f0f85
mohammadakashahammed <mohammadakashahammed@gmail.com>
2024-08-25 15:50:55 +0600

    Add a aug_25/practice1.py

---
+++ aug_25/practice1.py
@@ -0,0 +1,13 @@
+
+# if,elif, and else statement use
+
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+
+if a>=b:
+    print("A is biggest number: ", a)
+elif b>=c:
+    print("B is biggest number: ", b)
+else:
+    print("C is biggest number: ", c)
