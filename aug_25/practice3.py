2ece51429f505335221490283716a84ada77536c
mohammadakashahammed <mohammadakashahammed@gmail.com>
2024-08-25 16:34:43 +0600

    add a practice file

---
+++ aug_25/practice3.py
@@ -0,0 +1,16 @@
+
+# testList = ['item']
+# testList *= 3
+#
+# print(testList)
+
+
+# myList1 = ['item']
+# myList2 = 3
+# result = myList1 * myList2
+#
+# print(result)
+
+#
+print(my_var:="Hello")
+
